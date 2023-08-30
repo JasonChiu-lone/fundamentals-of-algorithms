5608f9e1b1337674fab509f39cdd08f05a1bd8c0
hatterm <uranuslight@outlook.com>
2023-08-30 14:16:44 +0800

    first commit

---
+++ week2/clock.cpp
@@ -0,0 +1,183 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+/**************************************************************************
+ * 2814:拨钟问题 http://bailian.openjudge.cn/practice/2814/
+ * 总时间限制: 1000ms 内存限制: 65536kB
+描述
+有9个时钟，排成一个3*3的矩阵。
+
+|-------|    |-------|    |-------|
+|       |    |       |    |   |   |
+|---O   |    |---O   |    |   O   |
+|       |    |       |    |       |
+|-------|    |-------|    |-------|
+    A            B            C    
+
+|-------|    |-------|    |-------|
+|       |    |       |    |       |
+|   O   |    |   O   |    |   O   |
+|   |   |    |   |   |    |   |   |
+|-------|    |-------|    |-------|
+    D            E            F    
+
+|-------|    |-------|    |-------|
+|       |    |       |    |       |
+|   O   |    |   O---|    |   O   |
+|   |   |    |       |    |   |   |
+|-------|    |-------|    |-------|
+    G            H            I    
+(图 1)
+现在需要用最少的移动，将9个时钟的指针都拨到12点的位置。共允许有9种不同的移动。如下表所示，每个移动会将若干个时钟的指针沿顺时针方向拨动90度。
+
+
+
+移动    影响的时钟
+ 
+ 1         ABDE
+ 2         ABC
+ 3         BCEF
+ 4         ADG
+ 5         BDEFH
+ 6         CFI
+ 7         DEGH
+ 8         GHI
+ 9         EFHI    
+输入
+9个整数，表示各时钟指针的起始位置，相邻两个整数之间用单个空格隔开。其中，0=12点、1=3点、2=6点、3=9点。
+输出
+输出一个最短的移动序列，使得9个时钟的指针都指向12点。按照移动的序号从小到大输出结果。相邻两个整数之间用单个空格隔开。
+样例输入
+3 3 0 
+2 2 2 
+2 1 2 
+样例输出
+4 5 8 9 
+**************************************************************************/
+
+int angles[9];
+
+class Move{
+    public:
+        short steps[9]; // 分别对应移动1-移动9的操作次数，step[i]取0-3
+        Move(){
+            for (int i = 0; i < 9; i++)
+                steps[i] = 0;
+        }
+        void operator++(){
+            bool carry = 1;
+            for (int digit = 0; digit < 9; digit++){
+                if (carry == 0) break;
+                steps[digit] += carry;
+                carry = steps[digit] > 3;
+                steps[digit] %= 4;
+            }
+        }
+};
+
+bool test(Move &move){
+    // copy angles
+    int angles_[9];
+    memcpy(angles_, angles, sizeof(angles));
+    // carry out move.steps to angles_
+    for (int d = 0; d < 9; d++){
+        for (int m = 0; m < move.steps[d]; m++){
+            switch(d){
+            case 0:
+                angles_[0]++;
+                angles_[1]++;
+                angles_[3]++;
+                angles_[4]++;
+                break;
+            case 1:
+                angles_[0]++;
+                angles_[1]++;
+                angles_[2]++;
+                break;
+            case 2:
+                angles_[1]++;
+                angles_[2]++;
+                angles_[4]++;
+                angles_[5]++;
+                break;
+            case 3:
+                angles_[0]++;
+                angles_[3]++;
+                angles_[6]++;
+                break;
+            case 4:
+                angles_[1]++;
+                angles_[3]++;
+                angles_[4]++;
+                angles_[5]++;
+                angles_[7]++;
+                break;
+            case 5:
+                angles_[2]++;
+                angles_[5]++;
+                angles_[8]++;
+                break;
+            case 6:
+                angles_[3]++;
+                angles_[4]++;
+                angles_[6]++;
+                angles_[7]++;
+                break;
+            case 7:
+                angles_[6]++;
+                angles_[7]++;
+                angles_[8]++;
+                break;
+            case 8:
+                angles_[4]++;
+                angles_[5]++;
+                angles_[7]++;
+                angles_[8]++;
+                break;
+            default:
+                break;
+            }
+        }
+        
+    }
+    for (int clock_ = 0; clock_ < 9; clock_++){
+        if (angles_[clock_] % 4 != 0) return false;
+    }
+    return true;
+}
+
+int main(){
+    for (int i = 0; i < 9; i++){
+        cin >> angles[i];
+    }
+    Move move;
+    while (true){
+        if (test(move)){
+            for (int c = 0; c < 9; c++){
+                if (move.steps[c]){
+                    for (int s = 0; s < move.steps[c]; s++){
+                        cout << c + 1
+                         << " ";
+                    }
+                }
+            }
+            break;
+        }
+        else{
+            ++move;
+        }
+    }
+    return 0;
+}
---
+++ week2/painter.cpp
@@ -0,0 +1,144 @@
+﻿/*
+2813:画家问题  http://bailian.openjudge.cn/practice/2813/
+总时间限制: 1000ms 内存限制: 65536kB
+描述
+有一个正方形的墙，由N*N个正方形的砖组成，其中一些砖是白色的，另外一些砖是黄色的。Bob是个画家，想把全部的砖都涂成黄色。但他的画笔不好使。当他用画笔涂画第(i, j)个位置的砖时， 位置(i-1, j)、 (i+1, j)、 (i, j-1)、 (i, j+1)上的砖都会改变颜色。请你帮助Bob计算出最少需要涂画多少块砖，才能使所有砖的颜色都变成黄色。
+
+![](http://media.openjudge.cn/images/2813_1.jpg)
+
+输入
+第一行是一个整数n (1≤n ≤15)，表示墙的大小。接下来的n行表示墙的初始状态。每一行包含n个字符。第i行的第j个字符表示位于位置(i,j)上的砖的颜色。“w”表示白砖，“y”表示黄砖。
+输出
+一行，如果Bob能够将所有的砖都涂成黄色，则输出最少需要涂画的砖数，否则输出“inf”。
+样例输入
+5
+wwwww
+wwwww
+wwwww
+wwwww
+wwwww
+样例输出
+15 
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+bool wall[15][15];
+int n;
+
+class Binary{
+    public:
+        bool bits[15];
+        Binary(){
+            memset(bits, 0, sizeof(bits));
+        }
+        void operator ++ (){
+            bool carry = 1;
+            for (int b = 0; b < 15; b++){
+                bits[b] ^= carry;
+                carry ^= bits[b];
+                if (!carry) break;
+            }
+        }
+        bool operator < (int right){
+            for (int b = 14; b >= 0; b--){
+                if (bits[b]){
+                    if (b < right - 1){
+                        return true;
+                    }
+                    else return false;
+                }
+            }
+            return false;
+        }
+        friend ostream &operator<< (ostream &os, Binary b){
+            for (int i = 0; i < n; i ++){
+                cout << b.bits[i] << " ";
+            }
+            return os;
+        }
+};
+
+void paint(bool wall_[15][15], int i, int j){
+    if (i >= n || j >= n) return;
+    // *********** change (i, j) ***********
+    wall_[i][j] ^= 1;
+    // change (i-1, j)
+    if (i >= 1) wall_[i-1][j] ^= 1;
+    // change (i+1, j)
+    if (i < n-1) wall_[i+1][j] ^= 1;
+    // change (i, j-1)
+    if (j >= 1) wall_[i][j-1] ^= 1;
+    // change (i, j+1)
+    if (j < n-1) wall_[i][j+1] ^= 1;
+}
+
+void print(bool wall_[15][15]){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << wall_[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "--------------" << endl;
+}
+
+int main(){
+    int ans = 0x7fffffff;
+    cin >> n;
+    char tmp;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> tmp;
+            wall[i][j] = (tmp == 'y');
+        }
+    }
+    // 首先枚举第一行是否paint
+    Binary bin;
+    for (int iter = 0; iter < 1 << n; ++bin, iter++){
+        // cout << "iter=" << iter << "\t" << bin << endl;
+        // 记录paint次数
+        int n_paint = 0;
+        // bin是枚举的第一行
+        bool wall_[15][15];
+        memcpy(wall_, wall, sizeof(wall));
+        for (int j = 0; j < n; j++){
+            if (bin.bits[j]){
+                paint(wall_, 0, j);
+                n_paint++;
+            }
+        }
+        // 然后根据第一行paint后的结果，paint至最后一行
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (!wall_[i-1][j]){
+                    paint(wall_, i, j);
+                    n_paint++;
+                }
+            }
+            // print(wall_);
+        }
+        // 判断最后一行是否全为1
+        bool full = true;
+        for (int j = 0; j < n; j++)
+            full &= wall_[n-1][j];
+        if (full) ans = min(ans, n_paint);
+    }
+    if (ans < 0x7fffffff) cout << ans << endl;
+    else cout << "inf" << endl;
+
+    return 0;
+}
